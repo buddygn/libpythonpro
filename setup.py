9f086e422f317335c07847e9777b4e2bc6130724
Geison <buddygn@gmail.com>
2020-12-01 00:12:56 -0300

    Configs pypi
    Release versão 0.2

--- setup.py
+++ setup.py
@@ -95,11 +95,11 @@ def find_package_data(
 
 
 PACKAGE = "libpythonpro"
-NAME = PACKAGE
+NAME = "libbuddy"
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
-AUTHOR = "Geison Nervo"
-AUTHOR_EMAIL = "geisonnervo@gmail.com"
-URL = "https://github.com/buddygn/libpythonpro"
+AUTHOR = "Renzo Nuccitelli"
+AUTHOR_EMAIL = "renzo@python.pro.br"
+URL = "https://github.com/pythonprobr/libpythonpro"
 VERSION = __import__(PACKAGE).__version__
 
 setup(
@@ -110,7 +110,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license="GNU AFFERO GENERAL PUBLIC LICENSE",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
