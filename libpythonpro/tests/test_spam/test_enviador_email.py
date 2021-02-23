04eb5ccda04e1cfc1d4a2687eea46cfdc9162fae
Geison <buddygn@gmail.com>
2021-02-22 22:54:00 -0300

    Conclusao testes automaticos

--- libpythonpro/tests/test_spam/test_enviador_email.py
+++ libpythonpro/tests/test_spam/test_enviador_email.py
@@ -6,14 +6,14 @@ from libpythonpro.spam.enviador_email import Enviador, EmailInvalido
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
-    
+
 
 @pytest.mark.parametrize(
     'destinatario',
     ['buddygn@gmail.com', 'aaaaa@aaa.com', 'bbbb@bb.com'])
 def test_remetente(destinatario):
     enviador = Enviador()
-    destinatatios = ['buddygn@gmail.com', 'aaaaa@aaa.com', 'bbbb@bb.com']
+    # destinatatios = ['buddygn@gmail.com', 'aaaaa@aaa.com', 'bbbb@bb.com']
     resultado = enviador.enviar(
         destinatario,
         'roger@naspsistemas.com.br',
@@ -30,7 +30,7 @@ def test_remetente(destinatario):
 def test_remetente_invalido(destinatario):
     enviador = Enviador()
     with pytest.raises(EmailInvalido):
-        resultado = enviador.enviar(
+        enviador.enviar(
             destinatario,
             'roger@naspsistemas.com.br',
             'Curso',
